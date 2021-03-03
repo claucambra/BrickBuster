851554bcaa5540a151f8ae46fac9804dff81a1aa
Claudio Cambra <claudio.cambra@gmail.com>
2021-03-03 23:52:52 +0100

    Ball actually moves now

--- Ball.gd
+++ Ball.gd
@@ -5,12 +5,12 @@ extends Area2D
 # var b = "text"
 
 export var speed = 200
+var direction = Vector2.UP
+onready var _initial_pos = position
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+#func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	position += speed * delta * direction
