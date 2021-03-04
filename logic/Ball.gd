5a651d377a36b8ecfe62f9f29e3bd00b1fa1abde
Claudio Cambra <claudio.cambra@gmail.com>
2021-03-04 01:10:55 +0100

    Simplified object types and structure

--- logic/Ball.gd
+++ logic/Ball.gd
@@ -1,16 +1,14 @@
-extends Area2D
+extends RigidBody2D
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-export var speed = 200
-var direction = Vector2.UP
-onready var _initial_pos = position
+export var speed = 150.0
 
 # Called when the node enters the scene tree for the first time.
-#func _ready():
+func _ready():
+	apply_impulse(Vector2(), Vector2(1, -1).normalized() * speed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	position += speed * delta * direction
+#func _process(delta):
