1ebf1baefa932fd805298b015d4973a026b743b8
Claudio Cambra <claudio.cambra@gmail.com>
2021-03-07 00:41:58 +0100

    Line tracks ball and mouse in real time

--- logic/LaunchInput.gd
+++ logic/LaunchInput.gd
@@ -5,31 +5,30 @@ extends Control
 # var b = "text"
 
 var drag_enabled = false
+onready var line = get_node("../LaunchLine")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	line.add_point(Vector2(0,0), 0)
+	line.add_point(Vector2(0,0), 1)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var ballCenter = get_node("../Ball").global_position
+	var mousePosition = get_global_mouse_position()
+	var direction = mousePosition - ballCenter
+	
+	line.visible = false
+	if drag_enabled:
+		line.visible = true
+		line.set_point_position(0, mousePosition)
+		line.set_point_position(1, ballCenter)
 
 func _on_Control_gui_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT:
 			drag_enabled = event.pressed
 			#print(drag_enabled)
-			var ballCenter = get_node("../Ball").global_position
-			var mousePosition = get_global_mouse_position()
-			#var direction = mousePosition - ballCenter
-			print(ballCenter)
-			print(mousePosition)
-			var line = get_node("../LaunchLine")
-			if event.pressed:
-				line.add_point(mousePosition, 0)
-				line.add_point(Vector2(299,299), 1)
-			else:
-				line.clear_points()
 
 
