fc190fed5fe8f500dbb49a67f7bc3e49557f8b20
Claudio Cambra <claudio.cambra@gmail.com>
2021-03-07 12:42:12 +0100

    Mouse input and launch line now work appropriately

--- logic/Ball.gd
+++ logic/Ball.gd
@@ -10,8 +10,8 @@ var held = false
 export var speed = 250.0
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	apply_impulse(Vector2(), Vector2(1, -1).normalized() * speed)
+#func _ready():
+#	apply_impulse(Vector2(), Vector2(1, -1).normalized() * speed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- logic/LaunchInput.gd
+++ logic/LaunchInput.gd
@@ -5,6 +5,7 @@ extends Control
 # var b = "text"
 
 var drag_enabled = false
+var firstClickPosition = Vector2(0,0)
 onready var line = get_node("../LaunchLine")
 
 # Called when the node enters the scene tree for the first time.
@@ -17,18 +18,26 @@ func _ready():
 func _process(delta):
 	var ballCenter = get_node("../Ball").global_position
 	var mousePosition = get_global_mouse_position()
-	var direction = mousePosition - ballCenter
+	var lineDirection = firstClickPosition - mousePosition
+	lineDirection = lineDirection.normalized()
 	
+	# Line drawing and touch place responsibilities
+	update() # Updates _draw func
 	line.visible = false
 	if drag_enabled:
 		line.visible = true
-		line.set_point_position(0, mousePosition)
-		line.set_point_position(1, ballCenter)
+		line.set_point_position(0, ballCenter)
+		line.set_point_position(1, lineDirection*10000)
+
+func _draw():
+	if drag_enabled:
+		draw_circle(firstClickPosition, 25, ColorN("black", 0.5))
 
 func _on_Control_gui_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT:
 			drag_enabled = event.pressed
+			firstClickPosition = get_global_mouse_position()
 			#print(drag_enabled)
 
 
