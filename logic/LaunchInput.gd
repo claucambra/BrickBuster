a5f5ce672465c7fee9d6b5932dcdb7223a1412ff
Claudio Cambra <claudio.cambra@gmail.com>
2021-03-11 00:14:43 +0100

    Score now is kept track of

--- logic/LaunchInput.gd
+++ logic/LaunchInput.gd
@@ -5,9 +5,12 @@ extends Control
 # var b = "text"
 
 var drag_enabled = false
-var firstClickPosition = Vector2(0,0)
-onready var ballScene = load("res://Ball.tscn")
-onready var ball = ballScene.instance()
+var launched = false
+var round_in_progress = false
+var score = 0
+var first_click_position = Vector2(0,0)
+onready var ball_scene = load("res://Ball.tscn")
+onready var ball = ball_scene.instance()
 onready var line = get_node("../LaunchLine")
 onready var wait = get_node("../LaunchTimer")
 
@@ -17,23 +20,24 @@ func _ready():
 	line.add_point(Vector2(0,0), 1)
 	add_child(ball)
 
-func launchBalls(direction, amount):
+func launch_balls(direction, amount):
 	for i in amount:
-		var nextBall = ballScene.instance()
-		add_child(nextBall)
-		nextBall.launch(direction)
+		var next_ball = ball_scene.instance()
+		add_child(next_ball)
+		next_ball.launch(direction)
 		wait.start()
 		yield(wait, "timeout")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	var ballCenter = ball.position
-	var mousePosition = get_global_mouse_position()
-	var lineDirection = firstClickPosition - mousePosition
-	lineDirection = lineDirection.normalized()
+	print(score)
+	var ball_center = ball.position
+	var mouse_position = get_global_mouse_position()
+	var line_direction = first_click_position - mouse_position
+	line_direction = line_direction.normalized()
 	
 	if Input.is_action_just_pressed("click"):
-		firstClickPosition = get_global_mouse_position()
+		first_click_position = get_global_mouse_position()
 		drag_enabled = true
 	
 	# Line drawing and touch place responsibilities
@@ -41,15 +45,25 @@ func _process(_delta):
 	line.visible = false
 	if drag_enabled:
 		line.visible = true
-		line.set_point_position(0, ballCenter)
-		line.set_point_position(1, lineDirection*10000)
+		line.set_point_position(0, ball_center)
+		line.set_point_position(1, line_direction*10000)
 		
 	if Input.is_action_just_released("click"): # Defined in input map
 		drag_enabled = false
-		self.launchBalls(lineDirection, 10)
+		self.launch_balls(line_direction, 10)
+		launched = true
+		
+	if get_child_count() > 1:
+		round_in_progress = true
+	elif launched == true:
+		launched = false
+		round_in_progress = false
+		score += 1
+	else:
+		round_in_progress = false
 
 func _draw():
 	if drag_enabled:
-		draw_circle(firstClickPosition, 25, ColorN("black", 0.5))
+		draw_circle(first_click_position, 25, ColorN("black", 0.5))
 
 
