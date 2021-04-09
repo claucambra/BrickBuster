3f7eda28d676943b8173cec1bda4d6f6bf424835
Claudio Cambra <claudio.cambra@gmail.com>
2021-04-09 16:55:28 +0200

    Fixed bug where scores button on main menu was enabled even if no scores were recorded in save file

--- logic/MainMenu.gd
+++ logic/MainMenu.gd
@@ -15,29 +15,32 @@ onready var popup_options_menu = $CanvasLayer/MainMenu/VBoxContainer/OptionsButt
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if not save_game.file_exists("user://savegame.save"):
-		$CanvasLayer/MainMenu/VBoxContainer/ContinueButton.visible = false
-		$CanvasLayer/MainMenu/VBoxContainer/ScoresButton.disabled = true
-	else:
+	if save_game.file_exists("user://savegame.save"):
 		save_game.open("user://savegame.save", File.READ)
 		while save_game.get_position() < save_game.get_len():
 			var node_data = parse_json(save_game.get_line())
 			var past_scores = node_data["past_scores"]
-			var top_score = past_scores.max()
-			var item_index = 0
-			for score in past_scores:
-				popup_score_list.add_item(String(score), null, false)
-				# We set an id number with item_index, we can also use item_index to access the right item
-				# Since list is currently in the order acquired from the save file.
-				popup_score_list.set_item_metadata(item_index, item_index)
-				if score == top_score:
-					popup_score_list.set_item_custom_bg_color(item_index,ColorN("red", 1))
-				item_index += 1
+			if past_scores.empty():
+				$CanvasLayer/MainMenu/VBoxContainer/ScoresButton.disabled = true
+			else:
+				var top_score = past_scores.max()
+				var item_index = 0
+				for score in past_scores:
+					popup_score_list.add_item(String(score), null, false)
+					# We set an id number with item_index, we can also use item_index to access the right item
+					# Since list is currently in the order acquired from the save file.
+					popup_score_list.set_item_metadata(item_index, item_index)
+					if score == top_score:
+						popup_score_list.set_item_custom_bg_color(item_index,ColorN("red", 1))
+					item_index += 1
 		var sort_options_button = $CanvasLayer/MainMenu/VBoxContainer/ScoresButton/PopupMenu/MarginContainer/VBoxContainer/HBoxContainer/SortOptionButton
 		sort_options_button.add_item("By attainment (asc)", 0)
 		sort_options_button.add_item("By attainment (desc)", 1)
 		sort_options_button.add_item("By score (asc)", 2)
 		sort_options_button.add_item("By score (desc)", 3)
+	else:
+		$CanvasLayer/MainMenu/VBoxContainer/ContinueButton.visible = false
+		$CanvasLayer/MainMenu/VBoxContainer/ScoresButton.disabled = true
 	
 	rng.randomize()
 	$TitleBrick.health = 90000
