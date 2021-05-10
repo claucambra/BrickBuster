1a1abc8fbc9427eafe9b782d9d70ae416259ad75
Claudio Cambra <claudio.cambra@gmail.com>
2021-05-11 01:06:25 +0200

    Mode selector game mode repository is now dynamically loaded

--- logic/ModeSelector.gd
+++ logic/ModeSelector.gd
@@ -5,10 +5,7 @@ extends Node
 # var a = 2
 # var b = "text"
 
-var game_modes = {
-	"standard": "GameStandard.gd",
-	"chaotic": "GameChaotic.gd"
-}
+var game_modes = {}
 var selected_game_mode = null
 
 var save_game = File.new()
@@ -17,6 +14,25 @@ onready var game_control = get_tree().get_root().get_node("MainGame")
 onready var board = get_parent().get_node("Board")
 
 func on_game_prepped():
+	var game_modes_dir = Directory.new()
+	var path = "res://logic/GameModes/"
+	game_modes_dir.open(path)
+	game_modes_dir.list_dir_begin()
+
+	var iterator = 0
+	while true:
+		var file_name = game_modes_dir.get_next()
+		if file_name == "":
+			break
+		elif not file_name.begins_with("."):
+			var game_mode_file = load(path + file_name)
+			var holder_node = Node2D.new()
+			holder_node.set_script(game_mode_file)
+			var game_mode_details = holder_node.get("game_mode_details")
+			game_modes[game_mode_details.name] = path + file_name
+			iterator += 1
+	game_modes_dir.list_dir_end()
+	
 	if save_game.file_exists("user://savegame.save"):
 		save_game.open("user://savegame.save", File.READ)
 		var node_data = parse_json(save_game.get_line())
@@ -28,7 +44,7 @@ func on_game_prepped():
 		elif game_mode == null:
 			selected_game_mode = "standard"
 		
-		var script = load("res://logic/GameModes/" + game_modes[selected_game_mode])
+		var script = load(game_modes[selected_game_mode])
 		board.set_script(script)
 		# _ready() func in a node's script listens for NOTIFICATION_READY
 		# Without this, the _ready() func won't be run
