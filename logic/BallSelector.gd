cf39ef2bbd1f3ad63e9dd69ad93b5b6f85e36d1c
Claudio Cambra <claudio.cambra@gmail.com>
2021-07-17 17:59:52 +0200

    Added margin to ball score menu label

--- logic/BallSelector.gd
+++ logic/BallSelector.gd
@@ -82,11 +82,11 @@ func on_Fadeout_finished(_anim_name):
 
 
 func _on_ItemList_item_selected(index):
-	$VBoxContainer/Label.text = ball_list.get_item_tooltip(index)
+	$VBoxContainer/MarginContainer/Label.text = ball_list.get_item_tooltip(index)
 
 
 func _on_TabContainer_tab_changed(tab):
 	if tab == 0:
-		$VBoxContainer/Label.visible = true
+		$VBoxContainer/MarginContainer/Label.visible = true
 	else:
-		$VBoxContainer/Label.visible = false
+		$VBoxContainer/MarginContainer/Label.visible = false
